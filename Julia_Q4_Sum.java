7b02ee86ef7ab0f434745733884059e195368198
Julia Nader <julianader4@gmail.com>
2023-09-15 22:31:28 +0300

    Edits in question 4: Sum

--- Julia_Q4_Sum.java
+++ Julia_Q4_Sum.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 import java.util.Vector;
-class HelloWorld {
+
+public class HelloWorld {
     public static void main(String[] args) {
-        int ans=0, i=0, size =0;
-        Scanner input = new Scanner(System.in); 
-        Vector<Integer> vec = new Vector<Integer>(size);
-        do{
-            vec.setSize(size);
-            System.out.print("Input " + (i+1) +": ");
+        int ans = 0, i = 0;
+        Scanner input = new Scanner(System.in);
+        Vector<Integer> vec = new Vector<Integer>();
+
+        do {
+            System.out.print("Input " + (i + 1) + ": ");
             vec.add(input.nextInt());
             i++;
-            size++;
             System.out.print("To add more = 1, To print = 0: ");
             ans = input.nextInt();
-        }while(ans==1);
-        int sum=0;
+        } while (ans == 1);
+
+        int sum = 0;
         System.out.println("Array Output:");
-        for (Integer num : vec) {         
+        for (Integer num : vec) {
             System.out.println("Number = " + num);
-            sum=sum+num;
+            sum += num;
         }
         System.out.println("Sum = " + sum);
     }
