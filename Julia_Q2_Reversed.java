d8175638062731ebab73150be8699eddbcf097a1
Julia Nader <julianader4@gmail.com>
2023-09-15 23:11:35 +0300

    Updating to Javascript

--- Julia_Q2_Reversed.java
+++ Julia_Q2_Reversed.java
@@ -1,13 +1,2 @@
-import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); 
-        System.out.print("Input: ");
-        String str = input.nextLine();
-        System.out.print("Output: ");
-        for (int i=str.length() - 1; i >= 0; i--){
-            System.out.print(str.charAt(i));
-        }
-    }
-}
+let input = prompt("Input: ");
+console.log("Output: " + input.split('').reverse().join(''));
